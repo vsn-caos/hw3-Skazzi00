027b2079fba571162cc305cca7730132bc6f07d1
Alex Eremin <eremin.casha@gmail.com>
2026-06-19 19:24:26 +0300

    solutions

--- problem1-fork-proc-print-numbers/solution.c
+++ problem1-fork-proc-print-numbers/solution.c
@@ -3,23 +3,39 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-// Программе передается аргумент — целое число N > 0.
-// Необходимо создать N-1 дополнительных процессов таким образом,
-// чтобы у каждого процесса было не более одного дочернего процесса.
-// Каждый из процессов должен вывести ровно одно число так,
-// чтобы на выходе получилась строка: 1 2 3 4 ... N
-// Между числами — ровно один пробел, строка завершается символом '\n'.
-
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <N>\n", argv[0]);
         return 1;
     }
 
-    int n = atoi(argv[1]);
+    int N = atoi(argv[1]);
+    int rank = 1;
 
-    // TODO: создайте цепочку из N процессов (каждый не более чем с одним потомком).
-    //       Каждый процесс выводит одно число. Порядок вывода должен быть 1 2 3 ... N.
+    while (rank < N) {
+        if (rank == 1)
+            printf("%d", rank);
+        else
+            printf(" %d", rank);
+        fflush(stdout);
+
+        pid_t pid = fork();
+        if (pid < 0) {
+            perror("fork");
+            return 1;
+        }
+        if (pid > 0) {
+            waitpid(pid, NULL, 0);
+            return 0;
+        }
+        rank++;
+    }
+
+    if (N == 1)
+        printf("1\n");
+    else
+        printf(" %d\n", rank);
+    fflush(stdout);
 
     return 0;
 }
--- problem4-sockets-tcp-client/solution.c
+++ problem4-sockets-tcp-client/solution.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-
-// Программе передаются два аргумента:
-//   argv[1] — IPv4-адрес сервера в десятичной записи (например, "127.0.0.1")
-//   argv[2] — номер порта
-//
-// Программа должна:
-//   1. Установить TCP-соединение с указанным сервером.
-//   2. В цикле читать со stdin целые знаковые числа в текстовом формате.
-//   3. Отправлять каждое число на сервер в бинарном виде (int32, Little Endian).
-//   4. Получать от сервера int32 LE в ответ и выводить его в stdout в текстовом виде.
-//   5. Если сервер закрыл соединение — завершиться с кодом возврата 0.
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
@@ -23,11 +13,44 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    // TODO: создайте TCP-сокет (AF_INET, SOCK_STREAM),
-    //       заполните struct sockaddr_in с помощью inet_aton/inet_pton,
-    //       подключитесь через connect,
-    //       реализуйте цикл чтения/отправки/приёма/вывода чисел.
-    //       Порядок байт — Little Endian (на x86/x86_64 это нативный порядок).
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("socket");
+        return 1;
+    }
 
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons((uint16_t)atoi(argv[2]));
+    if (inet_aton(argv[1], &addr.sin_addr) == 0) {
+        fprintf(stderr, "Invalid address: %s\n", argv[1]);
+        return 1;
+    }
+
+    if (connect(sockfd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+        perror("connect");
+        return 1;
+    }
+
+    int32_t n;
+    while (scanf("%d", &n) == 1) {
+        if (send(sockfd, &n, sizeof(n), 0) < 0) {
+            perror("send");
+            break;
+        }
+
+        int32_t resp;
+        ssize_t r = recv(sockfd, &resp, sizeof(resp), MSG_WAITALL);
+        if (r <= 0) {
+            close(sockfd);
+            return 0;
+        }
+
+        printf("%d\n", resp);
+        fflush(stdout);
+    }
+
+    close(sockfd);
     return 0;
 }
